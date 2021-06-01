42ba47b568064f03dc4adbc49ff41bbfe4550874
yha <bluefish1266@hanmail.net>
2021-06-01 12:37:19 +0900

    1

--- app.py
+++ app.py
@@ -1,13 +1,78 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from func import ck_idpw # 내가 만든 id pw 체크 함수
 app = Flask(__name__)
+import db
 
 @app.route('/naver') # 루트(/)는 뿌리
 def hello():
     return '안녕 나는 네이버야~'
 
-@app.route('/b')
-def hello2():
-    return 'Hello, World! bbbbbbb'
+@app.route('/form')
+def form():
+    return render_template("form.html")
+
+@app.route('/join')
+def join():
+    return render_template("join.html")
+
+@app.route('/join_action',methods=['GET', 'POST'])
+def join_action():
+    if request.method == 'GET':
+        return "다시 로그인 하세요."
+    else:
+        userid = request.form['userid']
+        pwd = request.form['pwd']
+        name = request.form['name']
+        phone = request.form['phone']
+        print(userid, pwd, name, phone)
+        db.insert_user(userid, pwd, name, phone)
+        return '회원가입 성공!!@'
+
+
+@app.route('/jo',methods = ['GET','POST'])
+def jo():
+    if request.method == 'GET':
+        return render_template('jo.html')
+        
+    else:
+        ID = request.form['ID']
+        PW = request.form['PW']
+        if ck_idpw(ID, PW):
+            return '반갑습니다~!!!'
+        else:
+            return '다시 로그인하세요.'
+
+
+
+
+
+
+@app.route('/action_page',methods=['GET', 'POST'])
+def action_page():
+    if request.method == 'GET':
+        return "나는 액션 GET페이지야"
+    else:
+        search = request.form['search']
+        return '''당신은 '{}'로 검색을 했습니다.<br>
+        결과를 보여드리겠습니다. 잠시만 기다려주세요!<br>
+        리스크쫙~~~
+        '''.format(search)
+
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+        
+    else:
+        userid = request.form['userid']
+        pwd = request.form['pwd']
+        print(userid,'pwd')
+        ret = db.get_idpw(userid, pwd)
+        return ck_idpw(ret)
+        #if 'qu' == '123' and 'qe' == '456':
+        #    return '안녕하세요 어서오세요'
+        #else:
+        #   return '다시 로그인하세요'
 
 # 웹브라우저에 http://127.0.0.1:5000/naver 
 # 위와같이 접속 하면 안녕 나는 네이버야~
--- func.py
+++ func.py
@@ -2,21 +2,32 @@ print("let's go game")
 
 # if 1이면 간다 2면 안간다
 
-# my = 1
+my = 1
 #여기를 짤라낸다
-def gogo(my):
+def login(my):
  
     if my == 1:
-        print("간다")
+        print("true")
 
     else:
-        print("안간다")
+        print("false")
+
+
 
 # 돈을 넣으면 2배가 뻥튀기
 
-# m = 1000
+m = 1000
 def coin(m):
     return m *2
 
-# mym = coin(1000)
-# print("내돈은 {0}". format(mym))
+mym = coin(1000)
+print("내돈은 {0}". format(mym))
+
+# 맞냐 틀렸냐
+
+def ck_idpw(ret):
+    #if id == 'a' and pw == '123':
+    if ret != None:
+        return '로그인 성공!!@'
+    else: 
+        return '가입 되지 않은 아이디나 패스워드.'
